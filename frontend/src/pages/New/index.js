4b25c714f6782315a3b5ea7af7f4a29c56b55fae
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-10-05 12:14:15 +0200

    web: page new-spot interface

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
-// import { Container } from './styles';
+import camera from '../../assets/camera.svg';
+
+import './styles.css';
 
 export default function New() {
+  const [ thumbnail, setThumbnail ] = useState(null);
+  const [ company, setCompany ] = useState('');
+  const [ techs, setTechs ] = useState('');
+  const [ price, setPrice ] = useState('');
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail])
+
+
+  function handleSubmit() {
+
+  }
+
   return (
-    <div />
+    <form onSubmit={handleSubmit}>
+      <label 
+        id="thumbnail"
+        style={{ backgroundImage: `url(${preview})`}}
+        className={thumbnail ? 'has-thumbnail' : ''}
+      >
+        <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
+        <img src={camera} alt="Select img"/>
+      </label>
+
+      <label htmlFor="company">EMPRESA *</label>
+      <input 
+        id="company"
+        placeholder="Sua empresa incrível"
+        value={company}
+        onChange={event => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="techs">TECHNOLOGIAS * <span>(separadas por vírgula)</span></label>
+      <input 
+        id="techs"
+        placeholder="Quais technologias usam"
+        value={techs}
+        onChange={event => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="price">VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span></label>
+      <input 
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={event => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">Cadastrar</button>
+    </form>
   );
 }
